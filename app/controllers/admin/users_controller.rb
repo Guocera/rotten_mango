4e1659f4df9d654fe5ea875ffdc5228b232d7ea8
Nick Yee <njsyee@gmail.com>
2016-02-03 14:56:48 -0700

    Only Admin can see admin page.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < ApplicationController
+  before_action :admin_access
+
   def index
     @users = User.all
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def admin_access
+    unless current_user && current_user.admin
+      flash[:alert] = "Admin only."
+      redirect_to movies_path
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
