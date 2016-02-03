1aff11e945ea548de25f5ee261ef11e92dab4f0f
Nick Yee <njsyee@gmail.com>
2016-02-03 12:50:12 -0700

    File upload and image shows.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203165632) do
+ActiveRecord::Schema.define(version: 20160203192434) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160203165632) do
     t.datetime "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "poster_image"
     t.string   "image"
   end
 
