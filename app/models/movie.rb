b641d34f1d33b4e2554cc1ba9ddcb8cba215eceb
Nick Yee <njsyee@gmail.com>
2016-02-03 14:17:41 -0700

    Added image validation.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,8 +13,8 @@ class Movie < ActiveRecord::Base
   validates :description,
     presence: true
 
-  # validates :poster_image_url,
-  #   presence: true
+  validates :image,
+    presence: true
 
   validates :release_date,
     presence: true
