1d8ff614332a81f2a9e4eeec5c79ac84953c9662
Nick Yee <njsyee@gmail.com>
2016-02-04 20:06:27 -0700

    Change sessions to resource instead of resources.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :users, only: [:new, :create, :update]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
